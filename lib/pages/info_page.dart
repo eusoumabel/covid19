c11e1b279e7a5126a1fedb991d97663e490a1b8d
estermabel <estermabel2@gmail.com>
2021-03-01 10:14:13 -0300

    info page ready to go. App icon ready and app name too

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -1,4 +1,9 @@
+import 'package:clipboard/clipboard.dart';
+import 'package:covid19/components/app_bar.dart';
+import 'package:covid19/components/native_loading.dart';
+import 'package:covid19/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_link_preview/flutter_link_preview.dart';
 
 class InfoPage extends StatefulWidget {
   @override
@@ -8,6 +13,100 @@ class InfoPage extends StatefulWidget {
 class _InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appBar(
+        context: context,
+        title: "Information",
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                Constants.kInformationString,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontSize: 15),
+              ),
+              _linkPreviewBuilder(
+                  previewString: Constants.kApiDocumentationUrl),
+              Text(
+                Constants.kAuthor,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontSize: 15),
+              ),
+              _linkPreviewBuilder(previewString: Constants.kAuthorGit),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _linkPreviewBuilder({String previewString}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
+      child: GestureDetector(
+        onTap: () {
+          _modal(copyString: previewString);
+        },
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: FlutterLinkPreview(
+            url: previewString,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future _modal({String copyString}) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(8, 8, 8, 16),
+          child: Container(
+            child: Wrap(
+              children: [
+                ListTile(
+                  title: Text(
+                    "Copy to clipboard.",
+                    style: Theme.of(context).textTheme.button.copyWith(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                  ),
+                  leading: IconTheme(
+                    data: Theme.of(context)
+                        .iconTheme
+                        .copyWith(color: Theme.of(context).primaryColor),
+                    child: Icon(Icons.copy),
+                  ),
+                  onTap: () => FlutterClipboard.copy(copyString).then(
+                    (value) => Navigator.pop(context),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Padding _onLoading() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+      child: NativeLoading(animating: true),
+    );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,9 +4,22 @@ class Constants {
   static String kBaseUrl = "https://api.covid19api.com";
   static String kFlagsUrl = "https://flagcdn.com";
 
-  //static String kApiKey = "bcd15b7346mshe40da8dad1ab385p1b99ebjsn720e5c3d3c90";
+  //static String kApiKey = "";
 
   static Options kOptions = Options(
     headers: {"X-Access-Token": "b5cf9dfd5-3449-485e-b5ae-70a60e997864"},
   );
+
+  static String kApiDocumentationUrl =
+      "https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest";
+
+  static String kInformationString = """
+  Covid-19
+
+  This is an app to keep track of the Covid-19 effect around the world. It was first created as a personal project to learn more about Flutter, but also to keep myself updated and others around me.
+  All the data used in this project is from an API provided by Postman.
+  """;
+
+  static String kAuthor = "Developer: Ester Mabel Silva Santos.";
+  static String kAuthorGit = "https://github.com/estermabel";
 }
